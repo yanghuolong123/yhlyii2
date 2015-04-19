6a7abf132c6cfd2a89567cdb3af5f4b1584d5468
yanghuolong <yanghuolong@97760597-0b9a-49ce-8cb1-1d1bfe9e9300>
2015-04-19 14:47:31 +0000

    yhlbbs

--- basic/common/AppController.php
+++ basic/common/AppController.php
@@ -12,8 +12,7 @@ class AppController extends Controller {
         if (Yii::$app->user->isGuest && isset($_COOKIE['0r1K_e4b6_auth'])) {
             $auth = $_COOKIE['0r1K_e4b6_auth'];
             $decode = authcode($auth);
-            $uid = intval(substr($decode, 33));
-            var_dump($auth, $decode, $uid);die;
+            $uid = intval(substr($decode, 33));            
             if ($uid) {
                 Yii::$app->user->login(Member::findUserById($uid), 3600 * 24 * 30);
             }
