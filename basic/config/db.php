cc439393648097374b67ec0845084ac12e0c2425
yanghuolong <yanghuolong@97760597-0b9a-49ce-8cb1-1d1bfe9e9300>
2015-04-19 13:45:08 +0000

    yhlbbs

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=yhl-bbs',
-    'username' => 'root',
-    'password' => '123456',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yhlbbs',
+    'username' => 'yhlbbs',
+    'password' => 'yhlbbs',
     'charset' => 'utf8',
     'tablePrefix' => 'pre_',
 ];
