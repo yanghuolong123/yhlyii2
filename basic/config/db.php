51d7f67ce80d7421d4c6886fdbc4f0494bbd863a
jason <yhl27ml@126.com>
2018-03-23 08:27:48 +0000

    Update db.php

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=yhlbbs',
-    'username' => 'feichangjuzu',
-    'password' => 'yhl-feichangjuzu',
+    'username' => 'root',
+    'password' => 'yanghuolong123',
     'charset' => 'utf8',
     'tablePrefix' => 'pre_',
 ];
